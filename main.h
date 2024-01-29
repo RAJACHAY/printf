b5d835660634e7fbdb1fba954d8138cba5b7837b
John-Lewis-Nkuubi <nlewisjohn@gmail.com>
2024-01-29 20:07:24 +0300

    main header file

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int putss(char *c);
+
+#endif
